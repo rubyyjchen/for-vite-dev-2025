1e36fd683d0dafcd2aff50918fe5330da1eb06a8
rubychen <rubyyjchen0102@gmail.com>
2025-07-31 22:31:59 +0800

    for github page

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/',
+  base:'/for-vite-dev-2025/',
   plugins: [
     vue(),
     vueDevTools(),
